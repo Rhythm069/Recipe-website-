fbf8cb9ed2c4da5d927f86702d2e4f1d28d0e8cd
Rhythm069 <rhythmacharya19@gmail.com>
2026-02-21 20:47:30 +0545

    Search-bar

---
+++ script.js
@@ -0,0 +1,11 @@
+function searchRecipes() {
+
+    let input = document.getElementById("searchInput").value.toLowerCase().trim();
+    let cards = document.querySelectorAll(".recipe-card");
+
+    cards.forEach(card => {
+        let name = card.dataset.name.toLowerCase();
+
+        card.style.display = name.includes(input) ? "block" : "none";
+    });
+}
