73c609051a2401d93f9c4501892e385dd8ddb763
Rhythm069 <rhythmacharya19@gmail.com>
2026-02-22 20:04:26 +0545

    about and contact

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ function searchRecipes() {
         behavior: "smooth"
     });
 }
+
+const contactForm = document.getElementById("contactForm");
+const popup = document.getElementById("popup");
+const closeBtn = document.querySelector(".close-btn");
+
+contactForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    popup.style.display = "flex"; 
+    contactForm.reset(); 
+});
+
+
+closeBtn.addEventListener("click", function () {
+    popup.style.display = "none";
+});
+
+
+window.addEventListener("click", function (e) {
+    if (e.target === popup) {
+        popup.style.display = "none";
+    }
+});
